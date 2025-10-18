858ea4899d2c80b52a74b0febca4a989d074db5a
huy120123 <phuoclop77@gmail.com>
2025-10-18 15:53:31 +0700

    Cập nhật products.php và index.php để hiển thị thông báo

--- src/api/export_stats_pdf.php
+++ src/api/export_stats_pdf.php
@@ -4,7 +4,8 @@ error_reporting(E_ALL);
 
 
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../../src/vendor/autoload.php';
+
 
 require_once __DIR__ . '/../config.php';
 
