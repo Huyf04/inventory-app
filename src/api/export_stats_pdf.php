913959d0f90307b365a14f191a045f68c92b4519
huy120123 <phuoclop77@gmail.com>
2025-10-18 15:34:11 +0700

    Cập nhật products.php và index.php để hiển thị thông báo

--- src/api/export_stats_pdf.php
+++ src/api/export_stats_pdf.php
@@ -1,6 +1,9 @@
 <?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+require_once __DIR__ . '/../../vendor/autoload.php';
 require_once __DIR__ . '/../config.php';
-require_once __DIR__ . '/../../vendor/autoload.php'; // đường dẫn chính xác đến vendor
 
 use Mpdf\Mpdf;
 
